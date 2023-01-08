0f4d7d8bba977e4e1374fa73a9909b781cc278b1
Lolo-collab-land <kiersnow@etud.insa-toulouse.fr>
2023-01-08 18:07:54 +0100

    paths modified

--- GIT-Final/app.py
+++ GIT-Final/app.py
@@ -4,33 +4,34 @@ import joblib
 import datasets
 import numpy as np
 
-model = joblib.load('./XGB_Target_model_saved.joblib')
+model = joblib.load('./XGB_model_saved.joblib')
 
 in1 = gr.inputs.Slider(minimum=0, maximum=44, step=1)#date
-in2 = gr.inputs.Slider(minimum=0, maximum=100, step=1) #stock
-in3 = gr.inputs.Dropdown(choices=['paris', 'copenhagen', 'madrid', 'rome', 'sofia', 'vilnius', 'vienna', 'amsterdam', 'valletta'])
+in2 = gr.inputs.Slider(minimum=1, maximum=100, step=1) #stock
+#in3 = gr.inputs.Dropdown(choices=['paris', 'copenhagen', 'madrid', 'rome', 'sofia', 'vilnius', 'vienna', 'amsterdam', 'valletta'])
 in4 = gr.inputs.Dropdown(choices=['romanian', 'swedish', 'maltese', 'belgian', 'luxembourgish',
        'dutch', 'french', 'finnish', 'austrian', 'slovakian', 'hungarian',
        'bulgarian', 'danish', 'greek', 'croatian', 'polish', 'german',
        'spanish', 'estonian', 'lithuanian', 'cypriot', 'latvian', 'irish',
        'italian', 'slovene', 'czech', 'portuguese'])
 in5 = gr.inputs.Radio(["0","1"])#mobile
-in6 = gr.inputs.Slider(minimum=0, maximum=1000, step=1) #hotel_id
+in6 = gr.inputs.Slider(minimum=0, maximum=998, step=1) #hotel_id
 
-inputs = [in1, in2, in3, in4, in5, in6]
+inputs = [in1, in2, in4, in5, in6]
+#inputs = [in1, in2, in3, in4, in5, in6]
 outputs = "text"
 
 
-def infer(date,stock,city,language,mobile,hotel_id):
+def infer(date,stock,language,mobile,hotel_id):#city
 
-  input_dataframe = pd.DataFrame([date,stock,city,language,mobile,hotel_id]).T
-  input_dataframe.columns = ["date","stock","city","language","mobile","hotel_id"]
+  input_dataframe = pd.DataFrame([date,stock,language,mobile,hotel_id]).T #city
+  input_dataframe.columns = ["date","stock","language","mobile","hotel_id"] #,"city"
 
   #ajout des features_hotels
   #hotels = pd.read_csv('/content/gdrive/My Drive/Defi-IA/features_hotels.csv', index_col=['hotel_id', 'city'])
   #input_complete = input_dataframe.join(hotels, on=['hotel_id', 'city'])
-  hotels = pd.read_csv('/content/gdrive/My Drive/Defi-IA/features_hotels.csv', index_col=['hotel_id'])
-  hotels = hotels.drop(['city'],axis=1)
+  hotels = pd.read_csv('./data/features_hotels.csv', index_col=['hotel_id'])
+  #hotels = hotels.drop(['city'],axis=1)
   input_complete = input_dataframe.join(hotels, on=['hotel_id'])
 
   #On affecte le bon type aux variables quantitatives
@@ -47,8 +48,8 @@ def infer(date,stock,city,language,mobile,hotel_id):
   input_complete["parking"]=pd.Categorical(input_complete["parking"],ordered=False)
   input_complete["pool"]=pd.Categorical(input_complete["pool"],ordered=False)
   input_complete["children_policy"]=pd.Categorical(input_complete["children_policy"],ordered=False)
-  
-  return "Predicted price : " + str(model.predict(input_complete)[0])
-
+  #print(input_complete)
+  return "You, as a/an "+str(language)+" speaker, chose to spend a night in hotel "+ str(hotel_id)+ " situated in "+str(input_complete["city"][0])+" in "+str(date)+" day(s). You supposed that "+str(input_complete["stock"][0])+ " rooms are still available. We estimate the price of your night at : " + str(model.predict(input_complete)[0])
+#infer(1,10,'finnish',3,999)
 
 gr.Interface(fn = infer, inputs = inputs, outputs = "text").launch(debug=True, share=True);
